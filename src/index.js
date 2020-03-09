b8d3ed92e987852964264bc0fc4605729307b382
julia-francais <ju.francais@gmail.com>
2020-03-09 17:43:29 +0100

    redux installed, first action dispatched, selectors on and Redux store subscription ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import store from "./store";
+import { getReadableStories } from "./selectors/story";
+import { STORY_ARCHIVE } from "./constants/actionTypes";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render() {
+  ReactDOM.render(
+    <App
+      stories={getReadableStories(store.getState())}
+      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />,
+    document.getElementById("root")
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+store.subscribe(render);
+render();
+
 serviceWorker.unregister();
