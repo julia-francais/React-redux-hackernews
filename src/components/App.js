f7e3aeb2999fbaef5ff4ad451158d46fe05f1d5f
julia-francais <ju.francais@gmail.com>
2020-03-10 11:51:07 +0100

    redux saga added connected to hackernews api

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./App.css";
 
 import Stories from "./Stories";
+import SearchStories from "./SearchStories";
 
 const App = () => (
   <div className="app">
+    <div classnmae="interactions">
+      <SearchStories />
+    </div>
     <Stories />
   </div>
 );
